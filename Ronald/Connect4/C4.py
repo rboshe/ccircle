f8bcb25aa50f7a26442ed22d8f54dc9d699e172d
rboshe <rboshe1@lsu.edu>
2017-07-17 16:14:59 -0500

    Merge branch 'master' of https://github.com/mathcircle/ccircle into ronald
    # Conflicts:
    #	pyproject/scenario04/readme.txt

--- Ronald/Connect4/C4.py
+++ Ronald/Connect4/C4.py
@@ -1,35 +1,67 @@
 import ccircle
-image = ccircle.Image('Connect4Board.png')
 window = ccircle.Window()
+window.toggleMaximized()
+
+print(window.getSize())
+
+def getColumn(window):  #  Should return the column that the mouse is in each time called
+    (x,y) = window.getMousePos()
+    if x > 0 and x < 228:
+        return 0
+    if x > 228 and x < 456:
+        return 1
+    if x > 456 and x < 684:
+        return 2
+    if x > 456 and x < 912:
+        return 3
+    if x > 912 and x < 1140:
+        return 4
+    if x > 1140 and x < 1368:
+        return 5
+    if x > 1368 and x < 1596:
+        return 6
 
 
 while window.isOpen():
     window.clear(0, 0, 1)
 
-    window.drawLine(300, 0, 300, 2000, 5, 1, 1, 1)
-    window.drawLine(600, 0, 600, 2000, 5, 1, 1, 1)
-    window.drawLine(800, 0, 800, 2000, 5, 1, 1, 1)
-    window.drawLine(1000, 0, 1000, 2000, 5, 1, 1, 1)
-    window.drawLine(1200, 0, 1200, 2000, 5, 1, 1, 1)
-    window.drawLine(1400, 0, 1400, 2000, 5, 1, 1, 1)
-    window.drawLine(0, 120, 2000, 120, 5, 1, 1, 1)
-    window.drawLine(0, 300, 2000, 300, 5, 1, 1, 1)
-    window.drawLine(0, 440, 2000, 440, 5, 1, 1, 1)
-    window.drawLine(0, 590, 2000, 590, 5, 1, 1, 1)
-    window.drawLine(0, 720, 2000, 720, 5, 1, 1, 1)
-    window.drawLine(30, 30, 200, 100, 4, 1, 0, 0)
-    window.drawLine(200, 30, 30, 100, 4, 1, 0, 0)
-    window.drawLine(30, 150, 200, 300, 4, 1, 1, 0)
-    window.drawLine(30, 280, 260, 150, 4, 1, 1, 0)
-    window.drawLine(30, 330, 290, 390, 4, 1, 0, 0)
-    window.drawLine(30, 400, 290, 320, 4, 1, 0, 0)
-    window.drawLine(50, 590, 260, 490, 4, 1, 1, 0)
-    window.drawLine(20, 460, 200, 590, 4, 1, 1, 0)
-    window.drawLine(30, 610, 260, 690, 4, 1, 0, 0)
-    window.drawLine(30, 680, 260, 610, 4, 1, 0, 0)
-    window.drawLine(30, 730, 270, 810, 4, 1, 1, 0)
-    window.drawLine(30, 800, 270, 730, 4, 1, 1, 0)
-    window.drawLine(300, 720, 600, 840, 4, 1, 1, 0)
-    window.drawLine(600, 720, 300, 840, 4, 1, 1, 0)
+    window.drawLine(228, 0, 228, 2000, 5, 1, 1, 1)
+    window.drawLine(456, 0, 456, 2000, 5, 1, 1, 1)
+    window.drawLine(684, 0, 684, 2000, 5, 1, 1, 1)
+    window.drawLine(912, 0, 912, 2000, 5, 1, 1, 1)
+    window.drawLine(1140, 0, 1140, 2000, 5, 1, 1, 1)
+    window.drawLine(1368, 0, 1368, 2000, 5, 1, 1, 1)
+    window.drawLine(0, 139, 2000, 139, 5, 1, 1, 1)
+    window.drawLine(0, 278, 2000, 278, 5, 1, 1, 1)
+    window.drawLine(0, 417, 2000, 417, 5, 1, 1, 1)
+    window.drawLine(0, 556, 2000, 556, 5, 1, 1, 1)
+    window.drawLine(0, 695, 2000, 695, 5, 1, 1, 1)
+    window.drawLine(0, 0, 228, 139, 4, 1, 0, 0)
+    window.drawLine(0, 139, 228, 0, 4, 1, 0, 0)
+    window.drawLine(0, 139, 228, 278, 4, 1, 1, 0)
+    window.drawLine(0, 278, 228, 139, 4, 1, 1, 0)
+    window.drawLine(0, 278, 228, 417, 4, 1, 0, 0)
+    window.drawLine(0, 417, 228, 278, 4, 1, 0, 0)
+    window.drawLine(0, 417, 228, 556, 4, 1, 1, 0)
+    window.drawLine(0, 556, 228, 417, 4, 1, 1, 0)
+    window.drawLine(0, 556, 228, 695, 4, 1, 0, 0)
+    window.drawLine(0, 695, 228, 556, 4, 1, 0, 0)
+    window.drawLine(0, 695, 228, 840, 4, 1, 1, 0)
+    window.drawLine(0, 840, 228, 695, 4, 1, 1, 0)
+    window.drawLine(228, 840, 456, 695, 4, 1, 1, 0)
+    window.drawLine(228, 695, 456, 840, 4, 1, 1, 0)
+    window.drawLine(228, 556, 456, 695, 4, 1, 1, 0)
+    window.drawLine(228, 695, 456, 556, 4, 1, 1, 0)
+    window.drawLine(228, 417, 456, 556, 4, 1, 0, 0)
+    window.drawLine(228, 556, 456, 417, 4, 1, 0, 0)
+    window.drawLine(456, 695, 684, 840, 4, 1, 1, 0)
+    window.drawLine(456, 840, 684, 695, 4, 1, 1, 0)
+    window.drawLine(456, 556, 684, 695, 4, 1, 0, 0)
+    window.drawLine(456, 695, 684, 556, 4, 1, 0, 0)
+    window.drawLine(684, 695, 912, 840, 4, 1, 0, 0)
+    window.drawLine(684, 840, 912, 695, 4, 1, 0, 0)
+
+    print(getColumn(window))
+    0
 
     window.update()
--- pyproject/lab_audio_synthesis/main.py
+++ pyproject/lab_audio_synthesis/main.py
@@ -8,13 +8,13 @@ window = ccircle.Window('Lab: Audio Synthesis')
 mysound = ccircle.Sound()
 
 def sawtooth(t, f):
-    return 2.0 * ((t * f) % 1.0) - 1.0
+    return 3.0 * ((t * f) % 9.0) - 9.0
 
 def sine(t, f):
-    return sin(2.0 * pi * t * f)
+    return sin(900.0 * pi * t * f)
 
 for i in range(44100):
     t = i / 44100
-    mysound.addSample(sine(t, 440))
+    mysound.addSample(sine(t, 10))
 mysound.play()
 time.sleep(1)
